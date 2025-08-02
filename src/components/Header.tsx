d99e7fa407843962d0631ab7f6506a85d3162693
eedu7 <mueedahmad067@gmail.com>
2025-08-02 16:33:49 +0500

    Added the userButton, when user is signed in

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+
+import { UserButton, useUser } from "@clerk/nextjs";
 
 import { Button } from "@/components/ui/button";
 
 export const Header = () => {
+    const { isSignedIn, user } = useUser();
+
     return (
         <header className="mx-auto flex w-full max-w-7xl items-center justify-between">
             <div>
@@ -11,17 +17,21 @@ export const Header = () => {
                     <h1 className="text-lg font-bold">JournelEdge</h1>
                 </Link>
             </div>
-            <div className="space-x-2">
-                <Button
-                    variant="link"
-                    asChild
-                >
-                    <Link href="/sign-in">Login</Link>
-                </Button>
-                <Button asChild>
-                    <Link href="/sign-up">Sign Up</Link>
-                </Button>
-            </div>
+            {!isSignedIn ? (
+                <div className="space-x-2">
+                    <Button
+                        variant="link"
+                        asChild
+                    >
+                        <Link href="/sign-in">Login</Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href="/sign-up">Sign Up</Link>
+                    </Button>
+                </div>
+            ) : (
+                <UserButton />
+            )}
         </header>
     );
 };
